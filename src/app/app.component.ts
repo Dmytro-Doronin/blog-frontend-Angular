4f7f9139aeb6036e24ec05698091328d30871b45
Dmytro-Doronin <doronindmytro623@gmail.com>
2024-03-10 18:41:01 +0100

    eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {BlogsModule} from "./blogs/blogs.module";
-import {HttpClientModule} from "@angular/common/http";
-
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { BlogsModule } from './blogs/blogs.module'
+import { HttpClientModule } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HttpClientModule, BlogsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'blog';
-
-
+  title = 'blog'
 }
