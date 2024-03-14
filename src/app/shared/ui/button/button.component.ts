c2977c663e7f65c1a8e82fe6e645bb03bbf3fa3c
Dmytro-Doronin <doronindmytro623@gmail.com>
2024-03-14 15:18:23 +0100

    some button fix

--- src/app/shared/ui/button/button.component.ts
+++ src/app/shared/ui/button/button.component.ts
@@ -1,15 +1,12 @@
-import {Component, Input} from '@angular/core';
-import {NgClass, NgSwitch} from "@angular/common";
+import { Component, Input } from '@angular/core'
+import { NgClass, NgSwitch } from '@angular/common'
 
 @Component({
   selector: 'blog-button',
   standalone: true,
-  imports: [
-    NgClass,
-    NgSwitch
-  ],
+  imports: [NgClass, NgSwitch],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
   @Input() buttonType: 'Primary' | 'Secondary' = 'Primary'
