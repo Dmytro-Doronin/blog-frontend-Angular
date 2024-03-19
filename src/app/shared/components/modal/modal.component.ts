dc266caf1857440247c74669bef9ac9b58d858c9
Dmytro Doronin <138795636+Dmytro-Doronin@users.noreply.github.com>
2024-03-19 22:42:53 +0100

    blog modal component

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {TypographyComponent} from "../../ui/typography/typography.component";
 
 @Component({
   selector: 'blog-modal',
   standalone: true,
-  imports: [],
+  imports: [
+    TypographyComponent
+  ],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
 export class ModalComponent {
+  @Output() closeModal = new EventEmitter<void>()
+  @Input() title: string = 'HGHG'
 
+  onClose() {
+    this.closeModal.emit()
+  }
 }
