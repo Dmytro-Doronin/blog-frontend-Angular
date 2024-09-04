e868d57af0b5941d90711c596a07c6f5dd7476c9
Dmytro Doronin <138795636+Dmytro-Doronin@users.noreply.github.com>
2024-09-04 18:24:13 +0200

    error component was started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module'
 import { BlogModule } from './blog/blog.module'
 import { HeaderModule } from './shared/header/header.module'
 import { ReactiveFormsModule } from '@angular/forms'
+import {ErrorComponent} from "./shared/components/error/error.component";
 
 @Component({
   selector: 'blog-root',
@@ -16,6 +17,7 @@ import { ReactiveFormsModule } from '@angular/forms'
     BlogModule,
     HeaderModule,
     ReactiveFormsModule,
+    ErrorComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
