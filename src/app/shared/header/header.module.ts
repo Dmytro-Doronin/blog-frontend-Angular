fe31ef1d4d68a14e1c5a816b148d91227253c42a
Dmytro-Doronin <doronindmytro623@gmail.com>
2024-09-28 19:52:41 +0200

    Change device fix with backend and router link

--- src/app/shared/header/header.module.ts
+++ src/app/shared/header/header.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HeaderComponentComponent } from './header-component/header-component.component'
+import { RouterLink } from '@angular/router'
 
 @NgModule({
   declarations: [HeaderComponentComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   exports: [HeaderComponentComponent],
 })
 export class HeaderModule {}
