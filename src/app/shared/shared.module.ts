c140830dcaaa9989c8894a972672fa42665cfe91
Dmytro-Doronin <doronindmytro623@gmail.com>
2024-11-07 16:27:30 +0100

    media query for auth

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -44,10 +44,10 @@ import { PostControlMenuComponent } from './components/post-control-menu/post-co
 import { CommentItemComponent } from './components/comment-item/comment-item.component'
 import { CommentControlMenuComponent } from './components/comment-control-menu/comment-control-menu.component'
 import { EditCommentFormComponent } from './components/comment-item/edit-comment-form/edit-comment-form.component'
-import {LoginMenuComponent} from "./components/login-menu/login-menu.component";
-import {ItemSimpleComponent} from "./components/item-simple/item-simple.component";
-import {ChromeIconComponent} from "./components/icons/chrome-icon/chrome-icon.component";
-import {DeviceItemComponent} from "./components/device-item/device-item.component";
+import { LoginMenuComponent } from './components/login-menu/login-menu.component'
+import { ItemSimpleComponent } from './components/item-simple/item-simple.component'
+import { ChromeIconComponent } from './components/icons/chrome-icon/chrome-icon.component'
+import { DeviceItemComponent } from './components/device-item/device-item.component'
 
 @NgModule({
   declarations: [
@@ -96,7 +96,7 @@ import {DeviceItemComponent} from "./components/device-item/device-item.componen
     LoginMenuComponent,
     ItemSimpleComponent,
     ChromeIconComponent,
-    DeviceItemComponent
+    DeviceItemComponent,
   ],
   imports: [
     CommonModule,
@@ -153,7 +153,7 @@ import {DeviceItemComponent} from "./components/device-item/device-item.componen
     LoginMenuComponent,
     ItemSimpleComponent,
     ChromeIconComponent,
-    DeviceItemComponent
+    DeviceItemComponent,
   ],
 })
 export class SharedModule {}
