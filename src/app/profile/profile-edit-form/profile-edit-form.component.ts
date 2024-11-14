3c40f9f1ce554ed59484ffc5d80d9f695b3682e3
Dmytro-Doronin <doronindmytro623@gmail.com>
2024-11-14 16:16:37 +0100

    profile change start

---
+++ src/app/profile/profile-edit-form/profile-edit-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'blog-profile-edit-form',
+  templateUrl: './profile-edit-form.component.html',
+  styleUrl: './profile-edit-form.component.scss',
+})
+export class ProfileEditFormComponent {}
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common'
 import { SharedModule } from '../shared/shared.module'
 import { ProfileRoutingModule } from './profile-routing.module'
 import { ProfilePageComponent } from './profile-page/profile-page.component'
+import { ProfileEditFormComponent } from './profile-edit-form/profile-edit-form.component'
 
 @NgModule({
-  declarations: [ProfilePageComponent],
+  declarations: [ProfilePageComponent, ProfileEditFormComponent],
   imports: [CommonModule, ProfileRoutingModule, SharedModule],
-  exports: [ProfilePageComponent],
+  exports: [ProfilePageComponent, ProfileEditFormComponent],
 })
 export class ProfileModule {}
