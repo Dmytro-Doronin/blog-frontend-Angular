919b2495b491e16b30737dc145e6da1b2c2e3b82
Dmytro-Doronin <doronindmytro623@gmail.com>
2024-11-17 15:12:00 +0100

    change url

--- src/app/core/services/services-variable.ts
+++ src/app/core/services/services-variable.ts
@@ -1,2 +1,2 @@
-// export const baseVercelUrl = 'https://blog-backend-nest.vercel.app'
-export const baseVercelUrl = 'http://localhost:3000'
+export const baseVercelUrl = 'https://blog-backend-nest.vercel.app'
+// export const baseVercelUrl = 'http://localhost:3000'
